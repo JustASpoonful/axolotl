cfdfe8cce69466562651929f8c11b5106835c372
Maxwell The Cat <105382091+epicstud@users.noreply.github.com>
2024-04-19 07:13:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ function toggleProxy() {
   document.body.classList.toggle('red-border', !isBlue);
   isBlue = !isBlue;
 }
+
+let variable = false; //false if page rediredcts, true if page dosnt.
+
+    if (!variable) {
+      window.location.href = 'https://wattle.vercel.app/warning.html';
+    }
+
+    document.getElementById('changeVariableBtn').addEventListener('click', function() {
+      variable = true;
+    });
+  
